35107c560c015d61c89a9c7bc75f39b098266714
MygJae <50861514+mygjae@users.noreply.github.com>
2024-02-20 17:05:12 +0900

    프록시 패턴과 데코레이터 패턴 - 구체 클래스 기반 프록시

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,30 @@
+package hello.proxy.pureproxy.concreteproxy;
+
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+
+
+}
+
---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,20 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreteClient {
+
+    private ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+    public void execute() {
+        concreteLogic.operation();
+    }
+
+
+}
+
